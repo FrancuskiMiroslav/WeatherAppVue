63189829963ffaf7409e56539eed3d75a0a67dca
Francuski Miroslav <francuski.miroslav@gmail.com>
2022-11-02 15:47:28 +0100

    base path fix

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env sh
 
+# abort on errors
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
 
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
+git checkout -B main
 git add -A
-git commit -m 'new gh-pages deploy'
-git push -f git@github.com:FrancuskiMiroslav/WeatherAppVue.git master:gh-pages
+git commit -m 'deploy to gh-pages'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:FrancuskiMiroslav/WeatherAppVue.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/WeatherAppVue/',
+  base: '/WeatherAppVue/',
   plugins: [vue()],
   resolve: {
     alias: {
